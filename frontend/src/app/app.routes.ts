dbbbfa7fe3b91670beedcc3e35794c806c67538e
IsraelSI11 <elplayerhd@hotmail.es>
2024-11-13 09:30:02 +0100

    Cambio de fuente y logo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { RegisterComponent } from './auth/register/register.component';
 import { AuthGuard } from './services/auth/auth.guard';
 import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
 import { InventoryFormComponent } from './inventory/inventory-form/inventory-form.component';
+import { IndexPageComponent } from './index-page/index-page.component';
 
 export const routes: Routes = [
+    {path: '', component: IndexPageComponent},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'inventory', component: InventoryListComponent, canActivate: [AuthGuard] },
