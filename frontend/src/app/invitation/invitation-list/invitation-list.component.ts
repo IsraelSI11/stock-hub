7ff4d4045739cf2f6213fd2c38957d373fc0f300
IsraelSI11 <elplayerhd@hotmail.es>
2024-11-14 12:50:45 +0100

    Cambiando invitaciones al componente card de materail

--- frontend/src/app/invitation/invitation-list/invitation-list.component.ts
+++ frontend/src/app/invitation/invitation-list/invitation-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { InvitationStateService } from '../../services/invitation/invitation-state.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-invitation-list',
   standalone: true,
+  imports: [MatProgressSpinnerModule, MatButtonModule, MatCardModule],
   providers: [InvitationStateService],
   templateUrl: './invitation-list.component.html',
   styleUrl: './invitation-list.component.css'
